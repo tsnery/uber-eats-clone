4481ae9bb95a4838a76f34882c5ecb5006410af8
Tainan Nery <nerytaynan@gmail.com>
2022-06-08 00:44:35 -0300

    feat: menu mock

---
+++ src/components/mock/menu.ts
@@ -0,0 +1,8 @@
+import { products } from "./products";
+
+export const menu = [
+  { name: "Starters", products },
+  { name: "Order Again", products },
+  { name: "Picked for you", products },
+  { name: "Gimbap Sushi", products },
+];
